a2aa2434f70898ceb45aa78ac59b509943ecdb5d
daadaadaah <daadaadaah@gmail.com>
2020-07-07 05:21:26 +0900

    Update HomePage for adding Link

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 export default function HomePage() {
   return (
     <div>
       <h1>Home</h1>
       <ul>
         <li>
-          <a href="/about">About</a>
+          <Link to="/about">About</Link>
         </li>
         <li>
-          <a href="/restaurants">Restaurants</a>
+          <Link to="/restaurants">Restaurants</Link>
         </li>
         <li>
-          <a href="/xxx">멸망의 길</a>
+          <Link to="/xxx">멸망의 길</Link>
         </li>
       </ul>
     </div>
